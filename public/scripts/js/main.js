71a52f86f87841c0886287fab38d5da7650370c2
IsaacAlves7 <isaac.alves0720@gmail.com>
2022-08-01 15:17:28 -0300

    7# Switch colors

--- public/scripts/js/main.js
+++ public/scripts/js/main.js
@@ -310,8 +310,6 @@ function bodyScrollingToggle(){
     }
 })();
 
-/*----------------------- Testimonial slider -------------------------*/
-
 /* ---------------- Hide all sections except active ---------------- */
 (()=>{
     // console.log("hi");
